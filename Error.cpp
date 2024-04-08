6c0bb15f5f1f358c3acbeda61fcf36ad936dbec1
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2024-04-08 20:48:04 +0800

    feat: handle error for address book

--- Error.cpp
+++ Error.cpp
@@ -1,2 +1,19 @@
 #include "pch.h"
+
 #include "Error.h"
+
+EmailerError::Error EmailerError::From(json &input) {
+    if (input.find("MailSend") != input.end()) {
+        return EmailerError::MailSend;
+    } else if (input.find("Curl") != input.end()) {
+        return EmailerError::Curl;
+    } else if (input.find("Http") != input.end()) {
+        return EmailerError::Http;
+    } else if (input.find("Serde") != input.end()) {
+        return EmailerError::Serde;
+    } else if (input.find("RemoteCall") != input.end()) {
+        return EmailerError::RemoteCall;
+    } else {
+        return EmailerError::Unknown;
+    }
+}
--- Error.h
+++ Error.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <exception>
 #include <iostream>
+#include <nlohmann/json.hpp>
 #include <string>
+
+using json = nlohmann::json;
 
 class SmtpError : public std::exception {
   private:
@@ -9,8 +12,21 @@ class SmtpError : public std::exception {
 
   public:
     SmtpError(const std::string msg) : message(msg) {
-        // std::cerr << message << std::endl;
+        std::cerr << message << std::endl;
     }
 
     const char *what() const noexcept override { return message.c_str(); }
 };
+
+namespace EmailerError {
+enum Error {
+    Curl = 0,
+    Http,
+    MailSend,
+    Serde,
+    RemoteCall,
+    Unknown,
+};
+
+Error From(json &input);
+} // namespace EmailerError
